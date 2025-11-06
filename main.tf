68dbddce29575ffa6840755247bd363b7671dc08
valhio <valentingerasimow@gmail.com>
2025-11-06 15:31:13 +0200

    feat: Configure S3 backend for Terraform state management

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "my-tf-state-sap-demo"
+    key            = "infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"  # optional but recommended
+    encrypt        = true
+  }
 }
 
 provider "aws" {
